1eea343a1af58ad364a5b252f6f258acfcf2d51f
김주영 <rlawndud7@nate.com>
2022-02-24 11:52:45 +0900

    [#2] 베이스 코드 작성 - SwiftLint를 pod로 적용, SwifterSwift/Alamofire 패키지로 적용

---
+++ WineApp/Pods/Target Support Files/Pods-WineApp/Pods-WineApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WineApp : NSObject
+@end
+@implementation PodsDummy_Pods_WineApp
+@end
---
+++ WineApp/WineApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WineApp
+//
+//  Created by JooYoung Kim on 2022/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
